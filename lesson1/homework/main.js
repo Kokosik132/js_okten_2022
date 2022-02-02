3fb4cadcae52674b0ba10ca608767bdebe785240
Kokosik132 <nbeyshchak@gmail.com>
2022-02-02 14:07:13 +0200

    lesson1_homework

---
+++ lesson1/homework/main.js
@@ -0,0 +1,170 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let q= `hello`;
+// let w= `owu`;
+// let e= `com`;
+// let r = `ua`;
+// let t = 1;
+// let y = 10;
+// let u = -999;
+// let i = 123;
+// let o = 3.14;
+// let p = 2.7;
+// let a = 16;
+// let s = true;
+// let d = false;
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(q);
+// console.log(w);
+// console.log(e);
+// console.log(r);
+// console.log(t);
+// console.log(y);
+// console.log(u);
+// console.log(i);
+// console.log(o);
+// console.log(p);
+// console.log(a);
+// console.log(s);
+// console.log(d);
+// alert(q);
+// alert(w);
+// alert(e);
+// alert(r);
+// alert(t);
+// alert(y);
+// alert(u);
+// alert(i);
+// alert(o);
+// alert(p);
+// alert(a);
+// alert(s);
+// alert(d);
+// document.write(q);
+// document.write(w);
+// document.write(e);
+// document.write(r);
+// document.write(t);
+// document.write(y);
+// document.write(u);
+// document.write(i);
+// document.write(o);
+// document.write(p);
+// document.write(a);
+// document.write(s);
+// document.write(d);
+// Переприсвоїти кожній змінній з завдання значення на довільне.
+// let q = 1;
+// let w = 2;
+// let e = 3;
+// let r = 4;
+// let t = 5;
+// let y = 6;
+// let u = 7;
+// let i = 8;
+// let o = 9;
+// let p = 10;
+// let a = 11;
+// let s = 12;
+// let d = 13;
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(q);
+// console.log(w);
+// console.log(e);
+// console.log(r);
+// console.log(t);
+// console.log(y);
+// console.log(u);
+// console.log(i);
+// console.log(o);
+// console.log(p);
+// console.log(a);
+// console.log(s);
+// console.log(d);
+// alert(q);
+// alert(w);
+// alert(e);
+// alert(r);
+// alert(t);
+// alert(y);
+// alert(u);
+// alert(i);
+// alert(o);
+// alert(p);
+// alert(a);
+// alert(s);
+// alert(d);
+// document.write(q);
+// document.write(w);
+// document.write(e);
+// document.write(r);
+// document.write(t);
+// document.write(y);
+// document.write(u);
+// document.write(i);
+// document.write(o);
+// document.write(p);
+// document.write(a);
+// document.write(s);
+// document.write(d);
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+// let firstName = `Nazar`
+// let middleName = `Beyshchak`
+// let lastName = `Olegovych`
+// let person = (firstName + middleName + lastName);
+// console.log(person)
+//
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let firstName = prompt(`FirstName`);
+// let middleName = prompt(`middleName`);
+// let year = prompt(`year`);
+// console.log(`Hello,`, firstName, middleName, `you are`, year, `old`)
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = `100`;
+// let c = true;
+//
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+// Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+// 5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+
+// let a = (5 < 6);
+// console.log(a);
+// let b = (5 > 6);
+// console.log(b);
+// let c = (5 == 6);
+// console.log(c);
+// let d = (5 === 6);
+// console.log(d)
+// let e = (10 == 10);
+// console.log(e);
+// let f = (10 === 10);
+// console.log(f);
+// let g = (10 !== 10);
+// console.log(g);
+// let x = (10 < 10);
+// console.log(x);
+// let z = (10 > 10);
+// console.log(z);
+// let zxz = (123 > `123`);
+// console.log(zxz);
+// let zzz = (123 == `123`);
+// console.log(zzz)
+
