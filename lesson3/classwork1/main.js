4fb529f5f11f5fd3994140ea199dcac3cfe3e0e2
Kokosik132 <nbeyshchak@gmail.com>
2022-02-07 15:40:19 +0200

    lesson3_classwork

---
+++ lesson3/classwork1/main.js
@@ -0,0 +1,5 @@
+<!--        template 1.1-->
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+for (i = 0; i < listOfItems.length; i++) {
+    document.write(`<ul> <li> ${listOfItems[i]} </li> </ul>`)
+}
