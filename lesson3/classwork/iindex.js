84aca9cc92abc7d5e0fd5140c15dfe98c59c71f2
Kokosik132 <nbeyshchak@gmail.com>
2022-02-07 15:11:48 +0200

    lesson3_classwork

---
+++ lesson3/classwork/iindex.js
@@ -0,0 +1,106 @@
+// зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+// 1. перебрати його циклом while
+// let kokos = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = 0;
+// while (i < kokos.length) {
+//     console.log(kokos[i])
+//     i++;
+// }
+// 2. перебрати його циклом for
+// for (i = 0; i < kokos.length; i++){
+//     console.log(kokos[i])
+// }
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+// let i = 0;
+// while (i < kokos.length) {
+//     i++;
+//     if (i % 2 !== 0){
+//         console.log(kokos[i])
+//     }
+//         }
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+// for (i = 0; i < kokos.length; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(kokos[i])
+//     }
+// }
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+// i = 0;
+// while (i < kokos.length) {
+//     i++;
+//     if (i % 2 === 0) {
+//         console.log(kokos[i])
+//     }
+// }
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+// for (i = 0; i < kokos.length; i++) {
+//     if (i % 2 === 0) {
+//         console.log(kokos[i])
+//     }
+// }
+// 7. замінити кожне число кратне 3 на слово "okten"
+// for (i = 0; i < kokos.length; i++) {
+//     if (kokos[i] % 3 === 0) {
+//         kokos[i] = `okten`;
+//         console.log(kokos[i])
+//     }
+// };
+// console.log(kokos)
+// 8. вивести масив в зворотньому порядку.
+// let kokos = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let reversed = kokos.reverse()
+// console.log(reversed)
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+// 1. перебрати його циклом while
+let kokos = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+let reversed = kokos.reverse()
+// for (i = 0; i < reversed.length; i++){
+//     console.log(reversed[i])
+// }
+// 2. перебрати його циклом for
+// for (i=0;i,reversed.length;i++){
+//     console.log(reversed[i])
+// }
+// 1. перебрати його циклом while
+// let i = 0;
+// while (i<reversed.length){
+//     console.log(reversed[i])
+//     i++
+// }
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+// let i = 0;
+// while (i < reversed.length) {
+//     i++;
+//     if (reversed[i] % 2 !== 0){
+//         console.log(reversed[i])
+//     }
+//         }
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+// for (i = 0; i < reversed.length; i++) {
+//     if (reversed[i] % 2 !== 0) {
+//         console.log(reversed[i])
+//     }
+// }
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+// let i = 0;
+// while (i<reversed.length){
+//     i++;
+//     if (reversed[i] % 2 === 0){
+//         console.log(reversed[i])
+//     }
+// }
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+// for (i=0;i<kokos.length;i++){
+//     if (kokos[i]%2===0){
+//         console.log(kokos[i])
+//     }
+// }
+// 7. замінити кожне число кратне 3 на слово "okten"
+// for (i=0;i<reversed.length;i++){
+//     if (reversed[i]%3===0){
+//         reversed[i] = `okten`;
+//         console.log(reversed[i])
+//     }
+// }
+// console.log(reversed)
+
