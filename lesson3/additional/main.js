748f8d983baeec1113072b85868c13424eee801d
Kokosik132 <nbeyshchak@gmail.com>
2022-02-17 09:49:34 +0200

    lesson3_additional

--- lesson3/additional/main.js
+++ lesson3/additional/main.js
@@ -40,7 +40,7 @@
 
 // 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
 // let sasd = []
-// for (i = 2; i < newsarr.length; i += 3) {
+// for (let i = 2; i < newsarr.length; i += 3) {
 //
 //     if (newsarr[i] % 2 === 0) {
 //
@@ -51,8 +51,10 @@
 // 5. Вивести кожен елемент масиву, сусід справа якого є парним
 // EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
 // let arr = [1, 2, 3, 5, 7, 9, 56, 8, 67]
-// for (i=0;i<arr.length;i++){
-//     console.log(arr[0],arr[5],arr[6])
+// for (let i=0;i<arr.length;i++){
+//     if (arr[i+1] % 2 === 0){
+//         console.log(arr[i])
+//     }
 // }
 
 // 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
@@ -60,7 +62,7 @@
 // let arrs = [100,250,50,168,120,345,188];
 // let sumarr = (arr) => {
 //     let sum = 0;
-//     for (i=0;i<arr.length;i++) {
+//     for (let i=0;i<arr.length;i++) {
 //         sum =+ arr[i]
 //     }
 //     return sum
@@ -70,7 +72,7 @@
 // 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
 // let sad = [123, 7, 54, 57, 8, 78, 75, 32, 4];
 // let newsarr = []
-// for (i = 0; i < sad.length; i++){
+// for (let i = 0; i < sad.length; i++){
 //     let numbers = sad[i] * 5
 // newsarr.push(numbers)
 // }
@@ -79,7 +81,7 @@
 // 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
 // let newarr = [12, `fasak`, true, false, 232, 1488, `yes`];
 // let arrrs = []
-// for (i = 0; i < newarr.length; i++) {
+// for (let i = 0; i < newarr.length; i++) {
 //     if (typeof newarr[i] === "number") {
 //         let numbers = newarr[i];
 //         arrrs.push(numbers)
@@ -108,60 +110,30 @@
 //     ]
 
 // Масиви:
-// let usersWithId = [
+// let arr1 = [
 //     {id: 1, name: 'vasya', age: 31, status: false},
 //     {id: 2, name: 'petya', age: 30, status: true},
 //     {id: 3, name: 'kolya', age: 29, status: true},
 //     {id: 4, name: 'olya', age: 28, status: false}
 // ];
 //
-// let citiesWithId = [
+// let arr2 = [
 //     {user_id: 3, country: 'USA', city: 'Portland'},
 //     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
 //     {user_id: 2, country: 'Poland', city: 'Krakow'},
 //     {user_id: 4, country: 'USA', city: 'Miami'}
 // ];
-// ///////////////////////////
-// let newsArr = []
-// for (let userWithIdElement of usersWithId) {
-//     if (userWithIdElement.id === 1)
-//         newsArr.push(`id: ${userWithIdElement.id} name: ${userWithIdElement.name} age:${userWithIdElement.age} status:${userWithIdElement.status}`)
+// // ///////////////////////////
+// let arr = [];
+// for (let user of arr1) {
+//     for (let city of arr2) {
+//         if (user.id === city.user_id){
+//             user.address = city
+//         }
+//     }
 // }
-//
-// for (let citiesWithIdElement of citiesWithId) {
-//     if (citiesWithIdElement.user_id === 1)
-//         newsArr.push(`user id: ${citiesWithIdElement.user_id} country: ${citiesWithIdElement.country} city: ${citiesWithIdElement.city}`)
-// }
-// console.log(newsArr)
-//
-//
-// for (let userWithIdElement of usersWithId) {
-//     if (userWithIdElement.id === 2)
-//         newsArr.push(`id: ${userWithIdElement.id} name: ${userWithIdElement.name} status: ${userWithIdElement.status}`)
-// }
-// for (let citiesWithIdElement of citiesWithId) {
-//     if (citiesWithIdElement.user_id === 2)
-//         newsArr.push(`user id: ${citiesWithIdElement.user_id} country: ${citiesWithIdElement.country} city: ${citiesWithIdElement.city}`)
-// }
-//
-//
-// for (let userWithIdElement of usersWithId) {
-//     if (userWithIdElement.id === 3)
-//         newsArr.push(`id: ${userWithIdElement.id} name: ${userWithIdElement.name} status: ${userWithIdElement.status}`)
-// }
-// for (let citiesWithIdElement of citiesWithId) {
-//     if (citiesWithIdElement.user_id === 3)
-//         newsArr.push(`user id: ${citiesWithIdElement.user_id} country: ${citiesWithIdElement.country} city: ${citiesWithIdElement.city}`)
-// }
-//
-// for (let userWithIdElement of usersWithId) {
-//     if (userWithIdElement.id === 4)
-//         newsArr.push(`id: ${userWithIdElement.id} name: ${userWithIdElement.name} status: ${userWithIdElement.status}`)
-// }
-// for (let citiesWithIdElement of citiesWithId) {
-//     if (citiesWithIdElement.user_id === 4)
-//         newsArr.push(`user id: ${citiesWithIdElement.user_id} country: ${citiesWithIdElement.country} city: ${citiesWithIdElement.city}`)
-// }
+// arr = arr1
+// console.log(arr)
 
 
 // Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
@@ -181,24 +153,27 @@
 // console.log(newarr)
 
 // Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
-let element = [`a`,`b`,`c`];
-// let elem = []
-// for (i=0;i<1;i++){
-//     console.log((`${element[0]}${element[1]}${element[2]}`))
+let element = [`a`, `b`, `c`];
+// let str = ``;
+// for (let i = 0; i < element.length; i++) {
+// str += element[i]
 // }
+// console.log(str)
 
 // Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
-
+// let str = ``;
 // let i = 0;
-// while (i<1){
-//     console.log((`${element[0]}${element[1]}${element[2]}`))
+// while (i<element.length){
+// str += element[i]
 //     i++
 // }
+// console.log(str)
 
 // Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
-
+// let str = ``
 // for (let string of element) {
-//     console.log((`${element[0]}${element[1]}${element[2]}`))
+//     str += string
 // }
+// console.log(str)
 //
 //
