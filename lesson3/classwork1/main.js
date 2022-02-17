d5c52b9f15eb90cd9abff78a9ab3940d155ac750
Kokosik132 <nbeyshchak@gmail.com>
2022-02-17 14:09:16 +0200

    lesson3_additional1

--- lesson3/classwork1/main.js
+++ lesson3/classwork1/main.js
@@ -90,32 +90,32 @@
 // }
 // document.write(`</div>`)
 
-// let products = [
-//     {
-//         title: 'milk',
-//         price: 22,
-//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-//     },
-//     {
-//         title: 'juice',
-//         price: 27,
-//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-//     },
-//     {
-//         title: 'tomato',
-//         price: 47,
-//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-//     },
-//     {
-//         title: 'tea',
-//         price: 15,
-//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-//     },
-// ];
-//
-// <!--        template 3.1-->
-// document.write(`<div>`)
-// for (prod of products) {
-// document.write(`<div class="product_card"><h3 class="product_title">${prod.title}. Price - ${prod.price}</h3><img src="${prod.image}" alt="Image"></div>`)
-// }
-// document.write(`</div>`)
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+<!--        template 3.1-->
+document.write(`<div>`)
+for (prod of products) {
+document.write(`<div class="product_card"><h3 class="product_title">${prod.title}. Price - ${prod.price}</h3><img src="${prod.image}" alt="Image"></div>`)
+}
+document.write(`</div>`)
